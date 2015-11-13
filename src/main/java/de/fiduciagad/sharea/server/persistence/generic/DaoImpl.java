76be40b3e432ce7279b0ef89cd4c3d339739b586
Matthias Weber <matthias.weber89@gmail.com>
2015-11-13 10:34:51 +0100

    Hinzufügen der fehlenden Abhängigkeiten

---
+++ src/main/java/de/fiduciagad/sharea/server/persistence/generic/DaoImpl.java
@@ -0,0 +1,15 @@
+package de.fiduciagad.sharea.server.persistence.generic;
+
+import java.io.Serializable;
+
+public class DaoImpl<T>  implements Dao<T> {
+
+	@Override
+	public T readById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
