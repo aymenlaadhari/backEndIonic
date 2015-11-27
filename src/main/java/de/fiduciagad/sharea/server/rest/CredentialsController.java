bbf7226066459ae911c659b02a83d593ed6cfc00
Adrian Rochau <adrian.rochau@gmail.com>
2015-11-27 09:53:02 +0100

    Fixed Credential-Service to return env vars.

--- src/main/java/de/fiduciagad/sharea/server/rest/CredentialsController.java
+++ src/main/java/de/fiduciagad/sharea/server/rest/CredentialsController.java
@@ -2,6 +2,7 @@ package de.fiduciagad.sharea.server.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,29 +10,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.fiduciagad.sharea.server.configuration.AppConfig;
 import de.fiduciagad.sharea.server.configuration.DatabaseConfiguration;
 import de.fiduciagad.sharea.server.dto.DBCredentials;
 
 @RestController
 public class CredentialsController {
-	
+
+	@Autowired
 	private DatabaseConfiguration dbconf;
+
 	private DBCredentials credentials;
+
 	private final AtomicLong counter = new AtomicLong();
-	
+
 	@CrossOrigin
 	@RequestMapping(value = "/credentials", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	@ResponseBody
-	public DBCredentials getCredentials(@RequestParam(value="user",required = false)String user, @RequestParam(value="password", required = false)String pw){
-		
+	public DBCredentials getCredentials(@RequestParam(value = "user", required = false) String user,
+			@RequestParam(value = "password", required = false) String pw) {
+
 		System.out.println("entered credentials Service");
-		
-		dbconf =new AppConfig().createDatabaseConfiguration();
-		
-			credentials = new DBCredentials(counter.getAndIncrement(), dbconf.getUrl());
-		
+
+		credentials = new DBCredentials(counter.getAndIncrement(), dbconf.getUrl());
 		return credentials;
 	}
-	
+
 }
