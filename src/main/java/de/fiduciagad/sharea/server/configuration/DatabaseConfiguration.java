80c82f96af905c5649f4751994d497ddb2a8149f
Matthias Weber <matthias.weber89@gmail.com>
2015-12-10 14:50:05 +0100

    debugging db

--- src/main/java/de/fiduciagad/sharea/server/configuration/DatabaseConfiguration.java
+++ src/main/java/de/fiduciagad/sharea/server/configuration/DatabaseConfiguration.java
@@ -1,16 +1,22 @@
 package de.fiduciagad.sharea.server.configuration;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class DatabaseConfiguration {
 	private String database;
 	private String password;
 	private String username;
 	private String url;
+	
+	private static final Log log = LogFactory.getLog(DatabaseConfiguration.class);
 
 	public DatabaseConfiguration(String username, String password, String database, String url) {
 		this.username = username;
 		this.password = password;
 		this.database = database;
 		this.url = url;
+		log.info("Using this database:" + database);
 	}
 
 	public String getUrl() {
