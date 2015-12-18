f7dd0218639b35616e39f834c65dd03a50cf87d3
xck1064 <xck1064@C3233CE7.fidudom.fiducia.de>
2015-12-18 12:13:01 +0100

    Removed precondition and simplified query in ShareRepository

--- src/main/java/de/fiduciagad/sharea/server/data/repository/ShareRepository.java
+++ src/main/java/de/fiduciagad/sharea/server/data/repository/ShareRepository.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import org.ektorp.ComplexKey;
 import org.ektorp.CouchDbConnector;
-import org.ektorp.Page;
-import org.ektorp.PageRequest;
 import org.ektorp.ViewQuery;
 import org.ektorp.support.CouchDbRepositorySupport;
 import org.ektorp.support.View;
@@ -36,7 +34,6 @@ public class ShareRepository extends CouchDbRepositorySupport<Share> {
 	@View(name = "by_startLocation_by_date", map = "classpath:/de/fiduciagad/sharea/server/data/repository/functions/by_location_by_date.js")
 	public List<Share> findByStartLocation(String startLocation, int limit) {
 		Preconditions.checkArgument(!Strings.isNullOrEmpty(startLocation), "Location cannot be empty.");
-		Preconditions.checkArgument(limit > 0, "A limit has to be greater than zero.");
 
 		// From now till the future!
 		Calendar nextYear = Calendar.getInstance();
@@ -45,12 +42,10 @@ public class ShareRepository extends CouchDbRepositorySupport<Share> {
 		ComplexKey startKey = ComplexKey.of(startLocation, now);
 		ComplexKey endKey = ComplexKey.of(startLocation, nextYear);
 
-		PageRequest pageRequest = PageRequest.firstPage(limit);
 		ViewQuery query = new ViewQuery().designDocId(stdDesignDocumentId).viewName("by_startLocation_by_date")
-				.includeDocs(true).startKey(startKey).endKey(endKey);
+				.includeDocs(true).startKey(startKey).endKey(endKey).limit(limit);
 
-		Page<Share> result = db.queryForPage(query, pageRequest, Share.class);
-		return result.getRows();
+		return db.queryView(query, Share.class);
 	}
 
 };
