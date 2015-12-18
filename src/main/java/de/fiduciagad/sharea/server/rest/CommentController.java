2d58c7e1ca2ca1d053e6b633b98a917ee8ed40b5
xck1064 <xck1064@C3233CE7.fidudom.fiducia.de>
2015-12-18 12:38:59 +0100

    It seems that I merged something. Don't know why but why not?

--- src/main/java/de/fiduciagad/sharea/server/rest/CommentController.java
+++ src/main/java/de/fiduciagad/sharea/server/rest/CommentController.java
@@ -1,10 +1,12 @@
 package de.fiduciagad.sharea.server.rest;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import de.fiduciagad.sharea.server.data.access.CommentManager;
 import de.fiduciagad.sharea.server.data.repository.dto.Comment;
+import de.fiduciagad.sharea.server.rest.dto.FindComments;
 
 @RestController
 public class CommentController {
@@ -28,6 +31,20 @@ public class CommentController {
 		commentManager.create(comment);
 
 		return Collections.singletonMap("id", comment.getId());
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "/api/v1/findComments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	@ResponseBody
+	public List<Comment> findComments(@RequestBody(required = true) FindComments findComments) {
+		return commentManager.findByShares(findComments.getShareId());
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/api/v1/comment/{id}", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
+	@ResponseBody
+	public Comment getShare(@PathVariable String id) {
+		return commentManager.get(id);
 	}
 
 }
