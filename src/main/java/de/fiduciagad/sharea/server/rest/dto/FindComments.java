c685c49ab393963feda31d72f08273b18febecf4
Matthias Weber <matthias.weber89@gmail.com>
2015-12-18 12:33:38 +0100

    extended commentcontroller

---
+++ src/main/java/de/fiduciagad/sharea/server/rest/dto/FindComments.java
@@ -0,0 +1,19 @@
+package de.fiduciagad.sharea.server.rest.dto;
+
+public class FindComments {
+	
+	private String shareId;
+	
+	public FindComments(){
+		
+	}
+
+	public String getShareId() {
+		return shareId;
+	}
+
+	public void setShareId(String shareId) {
+		this.shareId = shareId;
+	}
+
+}
