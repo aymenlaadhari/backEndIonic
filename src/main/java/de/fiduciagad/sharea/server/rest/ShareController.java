614de329eb282aab5e0629645651d2ff8899038a
xck1064 <xck1064@C3233CE7.fidudom.fiducia.de>
2015-12-18 10:32:00 +0100

    Fixed get share by ID

--- src/main/java/de/fiduciagad/sharea/server/rest/ShareController.java
+++ src/main/java/de/fiduciagad/sharea/server/rest/ShareController.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,7 +55,7 @@ public class ShareController {
 	@CrossOrigin
 	@RequestMapping(value = "/api/v1/share/{id}", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
 	@ResponseBody
-	public Share getShare(@RequestParam(required = true) String id) {
+	public Share getShare(@PathVariable String id) {
 		return shareManager.get(id);
 	}
 
