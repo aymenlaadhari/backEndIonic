ea81d501fb9a74b9d0ef0efeafe8a667159059de
xck1064 <xck1064@C3233CE7.fidudom.fiducia.de>
2016-01-07 15:43:11 +0100

    Added exception handling in services

---
+++ src/main/java/de/fiduciagad/sharea/server/rest/AbstractController.java
@@ -0,0 +1,29 @@
+package de.fiduciagad.sharea.server.rest;
+
+import org.ektorp.DocumentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This class contains some common methods required by the controllers.
+ *
+ * It will find the token and find the user for the token if required. It also
+ * prevents some exceptions to reach the Frontend or at least change their HTTP
+ * Code.
+ *
+ *
+ * @author xck1064
+ *
+ */
+@ControllerAdvice(annotations = RestController.class)
+public class AbstractController {
+
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	@ExceptionHandler(DocumentNotFoundException.class)
+	public void catchDocumentNotFoundException() {
+	}
+
+}
