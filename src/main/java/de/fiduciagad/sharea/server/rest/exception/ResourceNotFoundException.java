ff448d5a51080af427d829584e04ca3cc0c60561
xck1064 <xck1064@C3233CE7.fidudom.fiducia.de>
2016-01-07 14:27:49 +0100

    Added exception

---
+++ src/main/java/de/fiduciagad/sharea/server/rest/exception/ResourceNotFoundException.java
@@ -0,0 +1,26 @@
+package de.fiduciagad.sharea.server.rest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
